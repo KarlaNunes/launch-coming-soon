83383b143d3e1e9c65e3f075dcd05236e6c09087
Karla Nunes <karla.julyana1@gmail.com>
2022-04-07 10:35:16 -0300

    add countdown

---
+++ src/script.js
@@ -0,0 +1,32 @@
+const days = document.getElementById('days')
+const hours = document.getElementById('hours')
+const minutes = document.getElementById('minutes')
+const seconds = document.getElementById('seconds')
+
+const expectedDay = '10 Apr 2022'
+
+function countdown() {
+  const expectedDayDate = new Date(expectedDay)
+  const currentDate = new Date()
+
+  //diferença em segundos (tempo que falta em segundos)
+  const totalSeconds = (expectedDayDate - currentDate) / 1000
+
+  const daysLeft = Math.floor(totalSeconds / 3600 / 24)
+  const hoursLeft = Math.floor(totalSeconds / 3600) % 24
+  const minutesLeft = Math.floor(totalSeconds / 60) % 60
+  const secondsLeft = Math.floor(totalSeconds) % 60
+
+  days.innerHTML = daysLeft
+  hours.innerHTML = formatTime(hoursLeft)
+  minutes.innerHTML = formatTime(minutesLeft)
+  seconds.innerHTML = formatTime(secondsLeft)
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time
+}
+
+countdown()
+
+setInterval(countdown, 1000)
